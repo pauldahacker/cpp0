e52cf3f3989b247325815d2edf77e60a2ab747ad
Paul De Mascarel <pde-masc@car12s2.42barcelona.com>
2024-08-01 15:44:03 +0200

    ex0

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+char	to_upper(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 'a' + 'A');
+	return (c);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 0;
+	while (++i < argc)
+	{
+		j = -1;
+		while (argv[i][++j])
+			std::cout << to_upper(argv[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
+
+
